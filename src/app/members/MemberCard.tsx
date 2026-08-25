303ee3e9980a25f67cf4b49f269b89479cf090c7
Nimira43 <n.rabone72@aol.com>
2026-08-25 12:15:59 +0100

    MemberCard created.

---
+++ src/app/members/MemberCard.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { Member } from '../../../generated/prisma/client'
+import { Card, CardFooter } from '@heroui/react'
+import Image from 'next/image'
+import { calculateAge } from '@/lib/utils'
+
+type Props = {
+  member: Member
+}
+
+export default function MemberCard({member}: Props) {
+  return (
+    <Link href={`/members/${member.userId}`}>
+      <Card className='p-0 transition-all duration-300 hover:scale-105 hover:shadow-xl'>
+        <Image 
+          alt={member.name}
+          width={500}
+          height={500}
+          loading='eager'
+          sizes='(max-width: 768px) 100vw, 33vw'
+          src={member?.image || '/images/user.png'}
+          className='relative aspect-ratio object-cover rounded'
+        />
+        <CardFooter className='flex w-full justify-start absolute bottom-0 z-10 overflow-hidden bg-linear-to-t from-dark'>
+          <div className='flex flex-col text-light p-2'>
+            <span className='font-medium'>
+              {member.name}, {calculateAge(member.dateOfBirth)}
+            </span>
+            <span className='text-sm'>
+              {member.city}             
+            </span>
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  )
+}
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,17 +1,17 @@
 import { getMembers } from '@/server/actions/members'
+import MemberCard from './MemberCard'
 
 export default async function MembersPage() {
   const members = await getMembers()
   
   return (
-    <div>
-      <ul>
-        {members && members.map(member => (
-          <li key={member.id}>
-            {member.name}
-          </li>
-        ))}
-      </ul>
+    <div className='p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6'>
+      {members && members.map(member => (
+        <MemberCard 
+          key={member.id}
+          member={member}
+        /> 
+      ))}
     </div>
   )
 }
