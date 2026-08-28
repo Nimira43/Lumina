2fb08294e60dfa620401ae023de1b50b0f5c64ea
Nimira43 <n.rabone72@aol.com>
2026-08-28 22:39:06 +0100

    Member layout.

---
+++ src/app/members/[userId]/layout.tsx
@@ -0,0 +1,74 @@
+import { calculateAge } from '@/lib/utils'
+import { getMemberByUserId } from '@/server/actions/members'
+import { buttonVariants, Card, Separator } from '@heroui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export default async function Layout({
+  children,
+  params
+}: {
+  children: ReactNode,
+  params: Promise<{ userId: string }>
+}) {
+  const { userId } = await params
+  const member = await getMemberByUserId(userId)
+
+  if (!member) return notFound()
+  
+  return (
+    <div className='grid grid-cols-12 gap-5 h-[80vh]'>
+      <div className='col-span-3'>
+        <Card className='w-full mt-10 items-center h-[80vh]'>
+          <Image
+            alt={member.name}
+            width={500}
+            height={500}
+            loading='eager'
+            sizes='(max-width: 768px) 100vw, 33vw'
+            src={member?.image || '/images/user.png'}
+            className='relative aspect-ratio object-cover rounded'
+          />
+          <Card.Content>
+            <div className='flex flex-col items-center'>
+              <div className='text-2xl'>
+                {member.name}, {calculateAge(member.dateOfBirth)}
+              </div>
+              <div className='text-sm text-foreground/50'>
+                {member.city}, {member.country}
+              </div>
+            </div>
+            <Separator />
+            <div>
+              Member nav goes here
+            </div>
+          </Card.Content>
+          <Card.Footer className='w-full'>
+            <Link
+              href='/members'
+              className={buttonVariants({
+                variant: 'primary',
+                className: 'w-full'
+              })}
+            >
+              Go Back
+            </Link>
+          </Card.Footer>  
+        </Card>
+      </div>
+      <div className='col-span-9'>
+        <Card className='w-full mt-10 h-[80vh]'>
+          <Card.Header>
+            Section title
+          </Card.Header>
+          <Separator />
+          <Card.Content>
+            {children}
+          </Card.Content>
+        </Card>
+      </div>
+    </div>
+  )
+}
